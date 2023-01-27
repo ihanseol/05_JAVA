900b00387b3a924dabd7e42d18f8f6f7d439c5aa
ihanseol <hanseol33@naver.com>
2023-01-27 17:43:44 +0900

    add

---
+++ config/mygitpush.cmd
@@ -0,0 +1,27 @@
+::  @echo off
+:: @echo off
+
+cd /d d:/12_dev/01_Linux
+git status
+git add .
+git commit -a -m "add"
+git push origin master
+
+
+cd /d d:/12_dev/03_visual_lisp
+git status
+git add .
+git commit -a -m "add"
+git push origin master
+
+
+
+cd /d d:/12_dev/05_JAVA
+git status
+git add .
+git commit -a -m "add"
+git push origin master
+
+
+
+
