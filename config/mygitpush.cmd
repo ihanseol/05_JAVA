644959ae4870cac48b466ebb7afefcf81f9e4e37
ihanseol <hanseol33@naver.com>
2023-03-03 16:12:15 +0900

    add

--- config/mygitpush.cmd
+++ config/mygitpush.cmd
@@ -16,7 +16,15 @@ if "%CMD%" == "5" goto 05_JAVA
 
 @echo on
 
+
+
 :01_Linux
+@echo -------------------------------------------------
+@echo   01_Linux 
+@echo -------------------------------------------------
+@echo
+
+
 cd /d d:/12_dev/01_Linux
 
 git status
@@ -26,8 +34,13 @@ git push origin master
 goto exit
 
 
-
 :02_Excel
+
+@echo -------------------------------------------------
+@echo   02_Excel
+@echo -------------------------------------------------
+@echo
+
 cd /d d:/12_dev/02_Excel
 
 git status
@@ -36,7 +49,16 @@ git commit -a -m "add"
 git push origin master
 goto exit
 
+
+
 :03_visual_lisp
+
+@echo -------------------------------------------------
+@echo   03_visual_lisp
+@echo -------------------------------------------------
+@echo
+
+
 cd /d d:/12_dev/03_visual_lisp
 
 git status
@@ -47,6 +69,12 @@ goto exit
 
 
 :04_PyhtonDemoDojang
+@echo -------------------------------------------------
+@echo   04_PyhtonDemoDojang
+@echo -------------------------------------------------
+@echo
+
+
 cd /d d:/12_dev/04_PyhtonDemoDojang
 git status
 git add .
@@ -54,7 +82,17 @@ git commit -a -m "add"
 git push origin master
 goto exit
 
+
+
 :05_JAVA
+
+
+@echo -------------------------------------------------
+@echo   05_JAVA
+@echo -------------------------------------------------
+@echo
+
+
 cd /d d:/12_dev/05_JAVA
 git status
 git add .
@@ -66,11 +104,23 @@ goto exit
 
 :allpush
 
+@echo -------------------------------------------------
+@echo   d:/12_dev/01_Linux
+@echo -------------------------------------------------
+@echo
+
+
 cd /d d:/12_dev/01_Linux
 git status
 git add .
 git commit -a -m "add"
 git push origin master
+
+
+@echo -------------------------------------------------
+@echo   d:/12_dev/03_visual_lisp 
+@echo -------------------------------------------------
+@echo
 
 
 cd /d d:/12_dev/03_visual_lisp
@@ -80,11 +130,23 @@ git commit -a -m "add"
 git push origin master
 
 
+
+@echo -------------------------------------------------
+@echo   d:/12_dev/04_PyhtonDemoDojang 
+@echo -------------------------------------------------
+@echo
+
 cd /d d:/12_dev/04_PyhtonDemoDojang
 git status
 git add .
 git commit -a -m "add"
 git push origin master
+
+
+@echo -------------------------------------------------
+@echo   d:/12_dev/05_JAVA
+@echo -------------------------------------------------
+@echo
 
 
 cd /d d:/12_dev/05_JAVA
